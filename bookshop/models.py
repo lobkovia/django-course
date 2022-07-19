9cdc884ea782e52fd4cdfd0913d56ce24d2d8223
lobkovia <lobkovia@gmail.com>
2022-07-19 16:24:33 +0300

    initial commit

--- bookshop/models.py
+++ bookshop/models.py
@@ -4,6 +4,9 @@ from django.forms import CharField, IntegerField
 class Book(models.Model):
     title = models.CharField(max_length=70)
     rating = models.IntegerField()
+    is_best_selling = models.BooleanField(default=False)
+    author = models.CharField(max_length=100, null=True)
+    def __str__(self):
+        return f'{self.title} - {self.rating}'
 
 
-# Create your models here.
