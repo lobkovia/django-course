3556d0834a8dc9bd029d5ef90ea86f60aac46997
lobkovia <lobkovia@gmail.com>
2022-07-21 22:08:20 +0300

    initial commit

--- bookshop/admin.py
+++ bookshop/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.index_title = 'Мой охуенный заголовок'
+admin.site.site_header = 'Админка'
+
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title','author','rating','is_best_selling']
+    list_editable = ['rating', 'is_best_selling']
